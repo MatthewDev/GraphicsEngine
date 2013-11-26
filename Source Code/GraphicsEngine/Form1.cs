56a6da97d2b6df9480f870c95b1c0db55da6e88b
Andrew Kim <kim.andrew@live.com>
2013-11-26 00:19:41 -0800

    Added a bit and updated docs

--- Source Code/GraphicsEngine/Form1.cs
+++ Source Code/GraphicsEngine/Form1.cs
@@ -13,31 +13,27 @@ namespace GraphicsEngine
 {
 	public partial class Form1 : Form
 	{
-		private const int frameDelay = 1;
 		private Dictionary<string, SpriteBox> spriteList = new Dictionary<string, SpriteBox>();
 
-		/*	
-		//Sample Code
+		private void Init() //This method executes at program startup on UI thread
+		{
+			throw new NotImplementedException();
+		}
+
+		private const int frameDelay = 1;
+		private void FrameLoad() //This method executes constantly with an interval specified by frameDelay on seperate thread
+		{
+			throw new NotImplementedException();
+		}
 		
-		private void Init()
+		private void MouseDownHandler(object sender, MouseEventArgs e) //Fires when mouse is clicked on UI thread - remove if not needed
 		{
-			LoadSprite("img", Image.FromFile(@"C:\Users\Andrew\Desktop\img.png"), new Point(50, 50));
+			throw new NotImplementedException();
 		}
 
-		private void FrameLoad()
+		private void KeyDownHandler(object sender, KeyEventArgs e) //Fires when key is clicked on UI thread - remove if not needed
 		{
-			spriteList["img"].X += 5;
-		}
-		*/
-
-		private void Init()
-		{
-			
-		}
-
-		private void FrameLoad()
-		{
-
+			throw new NotImplementedException();
 		}
 
 		#region Internals
@@ -46,8 +42,30 @@ namespace GraphicsEngine
 			InitializeComponent();
 			Init();
 
+			MouseDown += MouseDownHandler;
+			KeyDown += KeyDownHandler;
+
 			Thread frameThread = new Thread(FrameThreadInit);
 			frameThread.Start();
+		}
+
+		private void LoadSprite(string name, Image img, Point loc)
+		{
+			spriteList[name] = new SpriteBox();
+			spriteList[name].Image = img;
+			spriteList[name].Location = loc;
+			spriteList[name].Size = img.Size;
+			spriteList[name].BackColor = Color.Transparent;
+			spriteList[name].MouseDown += MouseDownHandler;
+			Controls.Add(spriteList[name]);
+		}
+
+		private void RemoveSprite(string name)
+		{
+			Invoke(new Action(() => { Controls.Remove(spriteList[name]); }));
+			spriteList[name].MouseDown -= MouseDownHandler;
+			spriteList[name].Dispose();
+			spriteList.Remove(name);
 		}
 
 		private void FrameThreadInit()
@@ -59,20 +77,9 @@ namespace GraphicsEngine
 			}
 		}
 
-		private void LoadSprite(string name, Image img, Point loc)
+		private Point GetCursorPos()
 		{
-			spriteList[name] = new SpriteBox();
-			spriteList[name].Image = img;
-			spriteList[name].Location = loc;
-			spriteList[name].Size = img.Size;
-			
-			Controls.Add(spriteList[name]);
-		}
-
-		private void RemoveSprite(string name)
-		{
-			Invoke(new Action(() => { Controls.Remove(spriteList[name]); }));
-			spriteList.Remove(name);
+			return PointToClient(Cursor.Position);
 		}
 
 		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
